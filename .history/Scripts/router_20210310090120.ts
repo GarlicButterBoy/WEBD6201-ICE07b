ed9c3b30dbb441a127a46ae0ebdfdd45e9fd9d13
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-03-10 09:05:55 -0500

    updated router.js to typescript

--- .history/Scripts/router_20210310090120.ts
+++ .history/Scripts/router_20210310090120.ts
@@ -1,29 +1,40 @@
-"use strict";
+
 (function (core) {
-    m_activeLink: string;
-    m_routingTable: string[];
+
+    //Instance Variables
+    private m_
+
     class Router {
         // constructors
-        constructor() {
+        constructor() 
+        {
             this.ActiveLink = "";
         }
+
         // Public Properties (getters and setters)
-        get ActiveLink() {
+        get ActiveLink() 
+        {
             return this.m_activeLink;
         }
-        set ActiveLink(link) {
+
+        set ActiveLink(link) 
+        {
             this.m_activeLink = link;
         }
+
         // Public methods
+
         /**
          * Adds a new route to the Routing Table
          *
          * @param {string} route
          * @returns {void}
          */
-        Add(route) {
+        Add(route) 
+        {
             this.m_routingTable.push(route);
         }
+
         /**
          * This replaces the current Routing Table with a new one
          * Routes should begin with / character
@@ -31,9 +42,11 @@
          * @param {string} routingTable
          * @returns {void}
          */
-        AddTable(routingTable) {
+        AddTable(routingTable) 
+        {
             this.m_routingTable = routingTable;
         }
+
         /**
          * This method finds the index of the route in the routing table
          * otherwise it returns -1 if the route is not found
@@ -41,51 +54,62 @@
          * @param {string} route
          * @returns {number}
          */
-        Find(route) {
+        Find(route) 
+        {
             return this.m_routingTable.indexOf(route);
         }
+
         /**
          * This method removes a route from the Routing Table
          * It returns true if the route was successfully removed,
          * otherwise it returns false
-         *
+         * 
          * @param {string} route
          * @returns {boolean}
          */
-        Remove(route) {
+        Remove(route) 
+        {
             if (this.Find(route) > -1) {
                 this.m_routingTable.splice(this.Find(route), 1);
                 return true;
             }
             return false;
         }
+
         /**
-         * This method returns the routing table as a comma-separated string
+         * This method returns the routing table as a comma-separated string 
          *
          * @returns {string}
          */
-        ToString() {
+        ToString() 
+        {
             return this.m_routingTable.toString();
         }
     }
     core.Router = Router;
 })(core || (core = {}));
+
 let router = new core.Router();
-router.AddTable(["/",
-    "/home",
-    "/about",
-    "/services",
-    "/contact",
-    "/contact-list",
-    "/projects",
-    "/register",
-    "/login",
-    "/edit"]);
+router.AddTable(["/", 
+                 "/home", 
+                 "/about", 
+                 "/services", 
+                 "/contact", 
+                 "/contact-list", 
+                 "/projects", 
+                 "/register", 
+                 "/login", 
+                 "/edit"]);
+                
 let route = location.pathname; // alias for location.pathname
-if (router.Find(route) > -1) {
-    router.ActiveLink = (route == "/") ? "home" : route.substring(1);
+
+if(router.Find(route) > -1)
+{
+    router.ActiveLink = (route == "/") ? "home" : route.substring(1)
 }
-else {
+else
+{
     router.ActiveLink = "404";
 }
-//# sourceMappingURL=router.js.map
+
+
